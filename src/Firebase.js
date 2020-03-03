be0f4247490855782e6d10a62c77f09943821c32
Raju Khunt RK <rajukhunt15@gmail.com>
2020-03-03 16:38:05 +0530

    change the fire based cred

--- src/Firebase.js
+++ src/Firebase.js
@@ -1,17 +1,15 @@
 import * as firebase from 'firebase';
 
 const config = {
-	apiKey: 'AIzaSyDzUShJlg9_89orJfB2ZfFTWky-Hk3e85g',
-	authDomain: 'vuejs-firebase-realtime-chat.firebaseapp.com',
-	databaseURL: 'https://vuejs-firebase-realtime-chat.firebaseio.com',
+	apiKey: 'AIzaSyCSrwBhIW7zMmuXRHapAFBT04EytPJ0eN0',
+	authDomain: 'moneylending-d04a0.firebaseapp.com',
+	databaseURL: 'https://moneylending-d04a0.firebaseio.com',
 	projectId: 'vuejs-firebase-realtime-chat',
-	storageBucket: 'vuejs-firebase-realtime-chat.appspot.com',
-	messagingSenderId: '79720825114',
+	storageBucket: 'moneylending-d04a0.firebaseapp.com',
+	messagingSenderId: '1024087344489',
 	appId: '1:79720825114:web:4b9a282e7c8fab47b32ca3',
 	measurementId: 'G-LKHL4T19B3'
 };
-
-
 
 firebase.initializeApp(config);
 
